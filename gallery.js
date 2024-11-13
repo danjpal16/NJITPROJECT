896d740a19358076175a4db791b4e1ced63f2173
danjpal16 <525dpalagano@frhsd.com>
2024-11-13 10:29:21 -0500

    finished pretty much everything just have to style with css

--- gallery.js
+++ gallery.js
@@ -1,23 +1,25 @@
 let mCurrentIndex = 0 // Tracks the current image index
 let mImages = [] // Array to hold GalleryImage objects
 const mUrl = 'images.json' // Replace with actual JSON URL
-const mWaitTime = 5000 // Timer interval in milliseconds
+const mWaitTime = 8000 // Timer interval in milliseconds
 
 $(document).ready(() => {
+  $('.details').hide()
   //$('.details').hide() // Hide details initially
 
   // Call a function here to start the timer for the slideshow
-
+  startTimer();
   // Select the moreIndicator button and add a click event to:
   // - toggle the rotation classes (rot90 and rot270)
   // - slideToggle the visibility of the .details section
-
+  $('.moreIndicator').on('click', function() {
+    $('.moreIndicator').toggleClass('rot270');
+    $('.details').slideToggle(); 
+  })
   // Select the "Next Photo" button and add a click event to call showNextPhoto
-  $('#nextPhoto').on('click', () => {
-    swapPhoto;
-  });
+  $('#nextPhoto').on('click', showNextPhoto);
   // Select the "Previous Photo" button and add a click event to call showPrevPhoto
-
+  $('#prevPhoto').on('click', showPrevPhoto);
   // Call fetchJSON() to load the initial set of images
   fetchJSON()
 })
@@ -32,7 +34,8 @@ function fetchJSON () {
       console.log(data.images)
       mImages = data.images
       
-      $('#photo').attr('src', mImages[mCurrentIndex ].imgPath)
+      swapPhoto();
+      
 
     }
   });
@@ -46,18 +49,38 @@ function swapPhoto () {
   // Access mImages[mCurrentIndex] to update the image source and details
   // Update the #photo element's src attribute with the current image's path
   // Update the .location, .description, and .date elements with the current image's details
+  let imageData = mImages[mCurrentIndex];
+  $('#photo').attr("src",imageData.imgPath);
+  $('.name').text("Name: " + imageData.imgName);
+  $('.rank').text("Rank: " + imageData.imgRank);
+  $('.rating').text("Rating: " + imageData.imgRating);
+  $('.airtime').text("Air Time: " + imageData.imgAirtime);
+  $('.description').text("Description: " + imageData.imgDescription);
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
 function showNextPhoto () {
   // Increment mCurrentIndex and call swapPhoto()
   // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
+  mCurrentIndex++;
+    if (mCurrentIndex == mImages.length) {
+      mCurrentIndex = 0;
+    }
+    console.log(mCurrentIndex);
+    swapPhoto();
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto () {
   // Decrement mCurrentIndex and call swapPhoto()
   // Ensure it loops to the end if mCurrentIndex is less than 0
+  mCurrentIndex--;
+    if (mCurrentIndex < 0) {
+      mCurrentIndex = mImages.length - 1;
+    }
+
+    console.log(mCurrentIndex);
+    swapPhoto();
 }
 
 // Starter code for the timer function
@@ -65,4 +88,7 @@ function startTimer () {
   // Create a timer to automatically call `showNextPhoto()` every mWaitTime milliseconds
   // Consider using setInterval to achieve this functionality
   // Hint: Make sure only one timer runs at a time
+  setInterval(() => {
+    showNextPhoto();
+  }, mWaitTime);
 }
